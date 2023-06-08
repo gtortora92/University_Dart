8c2a7936d1b1a3a445e87e808a18399bfe308e8f
gtortora92 <tortoragiuseppe92@gmail.com>
2023-06-08 13:34:20 +0200

    Added class Course

---
+++ course.dart
@@ -0,0 +1,32 @@
+import 'teacher.dart';
+
+class Course {
+  Teacher _teacher;
+  String _code;
+  String _name;
+  String _degreeCourse;
+  String _description;
+
+  Course(this._teacher, this._code, this._name, this._degreeCourse,
+      this._description);
+
+  Teacher getTeacher() {
+    return _teacher;
+  }
+
+  String getCode() {
+    return _code;
+  }
+
+  String getName() {
+    return _name;
+  }
+
+  String getDegreeCourse() {
+    return _degreeCourse;
+  }
+
+  String getDescription() {
+    return _description;
+  }
+}
--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+import "course.dart";
 import "teacher.dart";
 void main() {
   print("First Commit");
@@ -5,4 +6,9 @@ void main() {
   Teacher t1 = Teacher("aaa", "gino", "gino", DateTime(1000,10,09), "ricercatore", "ING-INF/05");
   Teacher t2 = Teacher("aaa", "gino", "gino", DateTime(1000,10,09), "ricercatore", "ING-INF/05");
  
+ Course c1 = Course(t1, "64-365", "Algebra", "Informatica triennale", "Corso di base di algebra");
+
+ Teacher c1Teacher = c1.getTeacher();
+ print(c1Teacher.getCF());
+ print(c1Teacher.getName());
 }
